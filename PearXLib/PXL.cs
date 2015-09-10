32addf72887e439b12480c8505f103626ba7833a
mrAppleXZ <mr.applexz@gmail.com>
2015-09-10 13:57:39 +0600

    First commit

---
+++ PearXLib/PXL.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearXLib
+{
+    public class PXL
+    {
+        /* 
+         * =========================
+         * ======PearX Library======
+         * =====Open Source Lib=====
+         * =========pearx.ru========
+         * =========================
+         */
+
+
+
+
+
+
+        /// <summary>
+        /// Checks file's hash. Returns hash.
+        /// </summary>
+        /// <param name="path">Path to file.</param>
+        /// <returns></returns>
+        public static string hashCheck(string path)
+        {
+            using (FileStream fs = File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+        /// <summary>
+        /// Converts string to integer. This is alias for Convert.ToInt32(param).
+        /// </summary>
+        /// <param name="s">String</param>
+        /// <returns></returns>
+        public static int cTI(string s)
+        {
+            return Convert.ToInt32(s);
+        }
+
+        /// <summary>
+        /// Folder "My Documents".
+        /// </summary>
+        public static string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        /// <summary>
+        /// Folder "Application Data".
+        /// </summary>
+        public static string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        /// <summary>
+        /// Directory sepator.
+        /// </summary>
+        public static char s = Path.DirectorySeparatorChar;
+    }
+}
---
+++ PearXLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PearX Library")]
+[assembly: AssemblyDescription("A library by PearX Team.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PearX Team")]
+[assembly: AssemblyProduct("PearXLib")]
+[assembly: AssemblyCopyright("Copyright © PearX Lib 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0b9dbcc9-2083-4376-9f91-7408b75c3cfe")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
