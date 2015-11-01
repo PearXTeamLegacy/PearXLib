0144b2f84569597485939ad4868e358d5ad5b52d
mrAppleXZ <mr.applexz@gmail.com>
2015-11-01 14:34:42 +0500

    More char-int convertations

--- PearXLib/PXL.cs
+++ PearXLib/PXL.cs
@@ -49,7 +49,7 @@ namespace PearXLib
         /// <returns>True or false.</returns>
         public static bool isNumberKey(KeyPressEventArgs e)
         {
-            if(!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
             {
                 return true;
             }
@@ -149,7 +149,7 @@ namespace PearXLib
         /// <returns>Integer.</returns>
         public static int GetIntFromChar(char input)
         {
-            switch(input)
+            switch (input)
             {
                 case 'a':
                     return 1;
@@ -203,6 +203,112 @@ namespace PearXLib
                     return 25;
                 case 'z':
                     return 26;
+                case 'A':
+                    return 27;
+                case 'B':
+                    return 28;
+                case 'C':
+                    return 29;
+                case 'D':
+                    return 30;
+                case 'E':
+                    return 31;
+                case 'F':
+                    return 32;
+                case 'G':
+                    return 33;
+                case 'H':
+                    return 34;
+                case 'I':
+                    return 35;
+                case 'J':
+                    return 36;
+                case 'K':
+                    return 37;
+                case 'L':
+                    return 38;
+                case 'M':
+                    return 39;
+                case 'N':
+                    return 40;
+                case 'O':
+                    return 41;
+                case 'P':
+                    return 42;
+                case 'Q':
+                    return 43;
+                case 'R':
+                    return 44;
+                case 'S':
+                    return 45;
+                case 'T':
+                    return 46;
+                case 'U':
+                    return 47;
+                case 'V':
+                    return 48;
+                case 'W':
+                    return 49;
+                case 'X':
+                    return 50;
+                case 'Y':
+                    return 51;
+                case 'Z':
+                    return 52;
+                case ':':
+                    return 53;
+                case ';':
+                    return 54;
+                case '\'':
+                    return 55;
+                case '"':
+                    return 56;
+                case '[':
+                    return 57;
+                case ']':
+                    return 58;
+                case '{':
+                    return 59;
+                case '}':
+                    return 60;
+                case '-':
+                    return 61;
+                case '_':
+                    return 62;
+                case '+':
+                    return 63;
+                case '=':
+                    return 64;
+                case ')':
+                    return 65;
+                case '(':
+                    return 66;
+                case '*':
+                    return 67;
+                case '&':
+                    return 68;
+                case '?':
+                    return 69;
+                case '^':
+                    return 70;
+                case '%':
+                    return 71;
+                case '$':
+                    return 72;
+                case '#':
+                    return 73;
+                case '№':
+                    return 74;
+                case '@':
+                    return 75;
+                case '`':
+                    return 76;
+                case '~':
+                    return 77;
+                case '|':
+                    return 78;
+                case '/':
+                    return 79;
                 default:
                     return 0;
             }
@@ -264,6 +370,112 @@ namespace PearXLib
                     return 'y';
                 case 26:
                     return 'z';
+                case 27:
+                    return 'A';
+                case 28:
+                    return 'B';
+                case 29:
+                    return 'C';
+                case 30:
+                    return 'D';
+                case 31:
+                    return 'E';
+                case 32:
+                    return 'F';
+                case 33:
+                    return 'G';
+                case 34:
+                    return 'H';
+                case 35:
+                    return 'I';
+                case 36:
+                    return 'J';
+                case 37:
+                    return 'K';
+                case 38:
+                    return 'L';
+                case 39:
+                    return 'M';
+                case 40:
+                    return 'N';
+                case 41:
+                    return 'O';
+                case 42:
+                    return 'P';
+                case 43:
+                    return 'Q';
+                case 44:
+                    return 'R';
+                case 45:
+                    return 'S';
+                case 46:
+                    return 'T';
+                case 47:
+                    return 'U';
+                case 48:
+                    return 'V';
+                case 49:
+                    return 'W';
+                case 50:
+                    return 'X';
+                case 51:
+                    return 'Y';
+                case 52:
+                    return 'Z';
+                case 53:
+                    return ':';
+                case 54:
+                    return ';';
+                case 55:
+                    return '\'';
+                case 56:
+                    return '"';
+                case 57:
+                    return '[';
+                case 58:
+                    return ']';
+                case 59:
+                    return '{';
+                case 60:
+                    return '}';
+                case 61:
+                    return '-';
+                case 62:
+                    return '_';
+                case 63:
+                    return '+';
+                case 64:
+                    return '=';
+                case 65:
+                    return ')';
+                case 66:
+                    return '(';
+                case 67:
+                    return '*';
+                case 68:
+                    return '&';
+                case 69:
+                    return '?';
+                case 70:
+                    return '^';
+                case 71:
+                    return '%';
+                case 72:
+                    return '$';
+                case 73:
+                    return '#';
+                case 74:
+                    return '№';
+                case 75:
+                    return '@';
+                case 76:
+                    return '`';
+                case 77:
+                    return '~';
+                case 78:
+                    return '|';
+                case 79:
+                    return '/';
                 default:
                     return '!';
             }
