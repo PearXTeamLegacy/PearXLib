7ca2efe2dffe1e0a8a6dd747f71b2b1adb992e3f
mrAppleXZ <mr.applexz@gmail.com>
2016-03-28 17:40:28 +0300

    .

--- PearXLib/Engine/Flat/FlatBar.cs
+++ PearXLib/Engine/Flat/FlatBar.cs
@@ -8,6 +8,7 @@ namespace PearXLib.Engine.Flat
     public class FlatBar : XBar
     {
         private Color gc = Color.FromArgb(41, 128, 185);
+        private Color gc2 = Color.FromArgb(41, 128, 185);
         private Color bg = Color.FromArgb(37, 38, 41);
 
         public override Color GradientColor1
@@ -17,7 +18,7 @@ namespace PearXLib.Engine.Flat
 
         public override Color GradientColor2
         {
-            get { return gc; } set { gc = value; }
+            get { return gc2; } set { gc2 = value; }
         }
 
         public override Color BGColor
