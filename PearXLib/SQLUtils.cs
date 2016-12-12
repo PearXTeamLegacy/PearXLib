e101327247cb1d0469c87c13b61e582258e27e6a
mrAppleXZ <mr.applexz@gmail.com>
2016-12-12 09:38:58 +0500

    Moved SQLUtils to PearXLib

---
+++ PearXLib/SQLUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace PearXLib
+{
+	public static class SQLUtils
+	{
+		/// <summary>
+		/// Makes string safe for MySQL requests.
+		/// </summary>
+		/// <returns>An input string.</returns>
+		/// <param name="req">Formatted string.</param>
+		public static string FormatRequest(string req)
+		{
+			return req.Replace("'", @"\'").Replace("\"", "\\\"").Replace("%", @"\%").Replace("\n", @"\n").Replace("_", @"\_");
+		}
+	}
+}
